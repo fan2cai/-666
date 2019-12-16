ca3774a4185cc530fe172f304ef1271b2e4f1256
fcc <13703902520@163.com>
2019-12-16 13:51:56 +0800

    移动端适配案例

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="helloWorld">hello world</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .helloWorld {
+    width: 200px;
+    height: 200px;
+    background: pink;
+    margin: 0 auto;
+  }
+</style>
